7ebeb08bff13dc54b30be03cb4d74aeefe6ff27b
HARI RAJA <harihraja@gmail.com>
2018-08-28 18:38:32 -0700

    mlab mongodb cxn

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
 const app            = express();
 
@@ -10,7 +11,15 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+                        
+    // Make sure you add the database name and not the collection name
+    dbase = database.db("myriad_mdb")
+
+    require('./app/routes')(app, dbase);
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });             
+  })
+
