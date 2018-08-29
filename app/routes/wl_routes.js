d7dc5f3e33bf05b9f3ad93dd045b4173fef3610f
HARI RAJA <harihraja@gmail.com>
2018-08-28 19:10:36 -0700

    CRUD Ops work

--- app/routes/wl_routes.js
+++ app/routes/wl_routes.js
@@ -1,8 +1,54 @@
 // routes/wl_routes.js
 
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+
+    app.get('/watchlist/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('watchlist').findOne(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send(item);
+          } 
+        });
+    });
+
     app.post('/watchlist', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
-      });
+        const watchlist = { type: req.body.type, language: req.body.language, title: req.body.title };
+        db.collection('watchlist').insert(watchlist, (err, result) => {
+          if (err) { 
+            res.send({ 'error': 'An error has occurred' }); 
+          } else {
+            res.send(result.ops[0]);
+          }
+        });
+    });
+
+    app.delete('/watchlist/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('watchlist').remove(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send('Watchlist ' + id + ' deleted!');
+          } 
+        });
+    });
+
+    app.put('/watchlist/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const watchlist = { type: req.body.type, language: req.body.language, title: req.body.title };
+        db.collection('watchlist').update(details, watchlist, (err, result) => {
+          if (err) {
+              res.send({'error':'An error has occurred'});
+          } else {
+              res.send(watchlist);
+          } 
+        });
+    });
 };
