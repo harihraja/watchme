51b769cf4ef6780f51504bba523fa2e82f72d1d7
HARI RAJA <harihraja@gmail.com>
2018-09-19 13:26:43 -0700

    user id

--- app/routes/content_routes.js
+++ app/routes/content_routes.js
@@ -36,41 +36,41 @@ const CONTENT_INFO = [
 
 module.exports = function(app, db) {
 
-    app.get('/contentlist/:id/:language/:region/:type', (req, res) => {
+    app.get('/contentlist/:user/:language/:region/:type', (req, res) => {
+        const user = req.params.user;
         const lang = req.params.language;
         const region = req.params.region;
-        console.log(lang);
-        console.log(region);
+        const type = req.params.type;
 
-        var content_item = CONTENT_INFO.find(function(item) {
+        var content = CONTENT_INFO.find(function(item) {
           return item.language == lang && item.region == region && item.type == type;
         });
-        console.log(content_item.url);
+        console.log(content);
 
 
-        request(content_item.url, (err, response, body) => {
+        request(content.url, (err, response, body) => {
           if (err) { 
             res.send({'error':'An error has occurred'});
           }
-          $ = cheerio.load(body)
+          $ = cheerio.load(body);
 
           var movies = [];
           $('.FIL_right').each(function(i, elem) {
-            var movie = { watched : 'False' }
+            var movie = {}
             movie.title = $(this).find('h3').text();
             movie.rating = Number($(this).find('.star_count').text()).toFixed(1);
             movie.release_date = $(this).find('h4').text().split(" | ")[0];
+            movie.action = 'watch';
             movies[i] = movie;
 
             console.log(movies[i]);
           });
 
           movies = movies.filter(function(item) {
-            return Number(item.rating) >= Number(content_item.rating_limit);
+            return Number(item.rating) >= Number(content.rating_limit);
           });
           
           movies.sort(function (movie1, movie2) {
-          
             // Sort by rating
             // If the first item has a higher number, move it down
             // If the first item has a lower number, move it up
@@ -78,7 +78,7 @@ module.exports = function(app, db) {
             if (Number(movie1.rating) < Number(movie2.rating)) return 1;
           });
 
-          res.send({'url': content_item.url, 'movies': movies});
+          res.send({'user': user, 'content': content, 'movies': movies});
         });
 
     });
