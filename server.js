94bb33f3453a9849ee6202f6239a26171aa11c14
HARI RAJA <harihraja@gmail.com>
2018-12-11 14:33:18 -0800

    local & heroku together

--- server.js
+++ server.js
@@ -3,27 +3,31 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-
-// const db             = require('./config/db');
-// const MONGODB_URL   = db.url;
-const MONGODB_URL   = process.env.MONGOLAB_URI;
-
 const app            = express();
 
-const port = 8000;
+
+let db_url   = process.env.MONGOLAB_URI;
+if (db_url == null || db_url == "") {
+  const db = require('./config/db');
+  db_url = db.url;
+}
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(MONGODB_URL, (err, database) => {
+MongoClient.connect(db_url, (err, database) => {
     if (err) return console.log(err)
                         
     // Make sure you add the database name and not the collection name
     dbase = database.db("myriad_mdb")
 
     require('./app/routes')(app, dbase);
-    app.listen((process.env.PORT || port), () => {
-      // console.log('We are live on ' + port);
-      console.log('We are live on ' + process.env.PORT);
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
     });             
   })
 
